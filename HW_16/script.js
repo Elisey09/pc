17c5164ed5b3085b6bd0b99260f5a4d82df46268
Pc <pc@MacBook-Pro-Pc.local>
2021-06-17 15:06:23 +0300

    sdf

---
+++ HW_16/script.js
@@ -0,0 +1,10 @@
+
+const userTemplate = document.getElementById('usersList-template');
+const mainPageTemplate = document.getElementById('mainPage-template');
+
+const loginForm = new LoginForm(onLogin);
+const mainPage = new MainPage(userTemplate, mainPageTemplate);
+
+function onLogin(){
+  mainPage.init();
+}
